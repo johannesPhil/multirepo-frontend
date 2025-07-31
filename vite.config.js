a9661d81733b6720880329fba8d6f20f4f343957
johannesPhil <jay3pee@gmail.com>
2025-07-31 10:50:08 +0100

    add base path

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/test/",
+  // server: {
+  //   proxy: {
+  //     "/test/api": {
+  //       target: "http://localhost:5000",
+  //       changeOrigin: true,
+  //       rewrite: (path) => path.replace(/^\/test\/api/, "/api"),
+  //     },
+  //   },
+  // },
   plugins: [react()],
-})
+});
